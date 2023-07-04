b16793ea20c23fc713f30b867ed75f6ae7a9f118
Efezino Idisi <enduranceidisi13@gmail.com>
2023-07-04 02:33:36 +0100

    fix: tab spacing

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,22 @@ import GlobalStyles from './styles/global';
 import Home from './components/Home';
 import { TodosProvider } from './TodoContext';
 
-function App() {
-	const [dark, setTheme] = useState('dark');
-	const toggleTheme = () => {
-		dark === 'dark' ? setTheme('light') : setTheme('dark');
-	};
+function App () {
+  const [dark, setTheme] = useState('dark');
+  const toggleTheme = () => {
+    dark === 'dark' ? setTheme('light') : setTheme('dark');
+  };
 
-	return (
-		<TodosProvider>
-			<ThemeProvider theme={dark === 'dark' ? darkTheme : lightTheme}>
-				<GlobalStyles />
-				<div className='App'>
-					<Home toggle={toggleTheme} dark={dark} />
-				</div>
-			</ThemeProvider>
-		</TodosProvider>
-	);
+  return (
+    <TodosProvider>
+      <ThemeProvider theme={dark === 'dark' ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <div className='App'>
+          <Home toggle={toggleTheme} dark={dark} />
+        </div>
+      </ThemeProvider>
+    </TodosProvider>
+  );
 }
 
 export default App;
